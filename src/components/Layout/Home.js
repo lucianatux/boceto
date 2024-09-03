571cbdacd616e37523c64699f13bb82a5fc41b50
lucianatux <lucianatux@yahoo.com.ar>
2024-09-03 15:30:03 -0300

    carousel

--- src/components/Layout/Home.js
+++ src/components/Layout/Home.js
@@ -1,8 +1,50 @@
+import Carousel from 'react-bootstrap/Carousel';
+import forest from '../../assets/forest.jpg';
+import lake from '../../assets/lake.jpg';
+import mountain from '../../assets/mountain.jpg';
+
+
 export const Home = () => {
     return (
       <div className="home">
         <div>
-          <h1>Home</h1>
+          <Carousel fade className='carousel my-5'>
+      <Carousel.Item>
+      <img
+                            className="d-block w-100"
+                            src={lake}
+                            alt="First slide"
+                        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+                            className="d-block w-100"
+                            src={forest}
+                            alt="First slide"
+                        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+                            className="d-block w-100"
+                            src={mountain}
+                            alt="First slide"
+                        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
         </div>
       </div>
     )
