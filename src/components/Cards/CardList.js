bf4ddd94cb02f14b5aeca1f8b299419c78f15ecc
lucianatux <lucianatux@yahoo.com.ar>
2024-09-08 17:58:08 -0300

    Update CardList.js

--- src/components/Cards/CardList.js
+++ src/components/Cards/CardList.js
@@ -1,26 +1,61 @@
+import { useState, useEffect } from "react";
 import { CardForm } from "./CardForm";
 import { db } from '../../Firebase';
-import { collection, doc, setDoc } from "firebase/firestore"; // Importar las funciones necesarias
+import { collection, doc, setDoc, onSnapshot } from "firebase/firestore"; 
 
 export const CardList = () => {
+
+  const [cards, setCards] = useState([]);
   
   // Función para agregar o editar una tarjeta
   const addOrEditCard = async (cardObject) => {
     try {
-      const cardsCollectionRef = collection(db, 'cards'); // Acceder a la colección 'cards'
-      const newDocRef = doc(cardsCollectionRef); // Crear una referencia a un nuevo documento
-      await setDoc(newDocRef, cardObject); // Guardar el documento en la colección
+      const cardsCollectionRef = collection(db, 'cards'); 
+      const newDocRef = doc(cardsCollectionRef); 
+      await setDoc(newDocRef, cardObject); 
       console.log("New card added");
     } catch (error) {
       console.error("Error adding document: ", error);
     }
   }
 
+ // Función para obtener todas las tarjetas
+  const getCards = () => {
+    try {
+      const cardsCollectionRef = collection(db, 'cards'); 
+  
+      onSnapshot(cardsCollectionRef, (querySnapshot) => {
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          docs.push({...doc.data(), id:doc.id});
+        });
+        setCards(docs);
+      });
+      
+    } catch (error) {
+      console.error("Error fetching cards in real-time: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getCards();
+  }, []);
+
+ 
+
   return (
     <div className="card-list">
       <CardForm addOrEditCard={addOrEditCard} />
-      <div>
-        <h1>Card List</h1>
+      <div className="col-md-8">
+      {cards.map((card) =>(
+        <div className="card mb-1">
+          <div className="card-body">
+          <h4>{card.name}</h4>
+          <p>{card.description}</p>
+          <a href={card.url} target="_blank" rel="noreferrer">Go to website</a>
+          </div>
+        </div>
+      ))}
       </div>
     </div>
   )
